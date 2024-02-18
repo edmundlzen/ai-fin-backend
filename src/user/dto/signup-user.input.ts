e6641f4caccec219bd608d10607e5f504aa9894f
Edmund <edmundlzen@gmail.com>
2024-02-18 14:33:35 +0800

    Add validation to CreateUserInput

--- src/user/dto/signup-user.input.ts
+++ src/user/dto/signup-user.input.ts
@@ -1,19 +1,37 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
+  @IsEmail()
   @Field(() => String, { description: 'Email of the user' })
   email: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
   @Field(() => String, { description: 'Unhashed password of the user' })
   password: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   @Field(() => String, { description: 'Username of the user' })
   username: string;
 
+  @IsString()
+  @IsNotEmpty()
   @Field(() => String, { description: 'Phone number of the user' })
   phone: string;
 
+  @IsNumber()
+  @IsNotEmpty()
   @Field(() => Int, { description: 'Birth year of the user' })
   birthYear: number;
 }
