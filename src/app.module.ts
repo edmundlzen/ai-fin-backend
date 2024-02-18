0f6e48947f5eb1136f2e5f195c6984ce346d1dbc
Edmund <edmundlzen@gmail.com>
2024-02-18 13:37:44 +0800

    Refactor GraphQL configuration and remove user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { UserModule } from './user/user.module';
-import { join } from 'path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      typePaths: ['./**/*.graphql'],
-      definitions: {
-        path: join(process.cwd(), 'src/graphql.ts'),
-        outputAs: 'class',
-      },
+      autoSchemaFile: true,
     }),
-    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
