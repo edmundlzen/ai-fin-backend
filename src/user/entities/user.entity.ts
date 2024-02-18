ba9293ce009cc517525787bb26b5b37b71385f68
Edmund <edmundlzen@gmail.com>
2024-02-18 14:39:44 +0800

    Refactor User entity to include Prisma type and exclude password hash

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,7 +1,15 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType } from '@nestjs/graphql';
+import { User as UserPrismaType } from '@prisma/client';
+import { Exclude } from 'class-transformer';
 
 @ObjectType()
-export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+export class User implements UserPrismaType {
+  id: string;
+  email: string;
+  username: string;
+  phone: string;
+  birth_year: number;
+
+  @Exclude()
+  password_hash: string;
 }
