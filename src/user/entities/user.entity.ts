5558e511fe62edc279b507934893eb6ab3fc1442
Edmund <edmundlzen@gmail.com>
2024-02-18 15:08:00 +0800

    Add GraphQL field decorators to User entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,13 +1,22 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { User as UserPrismaType } from '@prisma/client';
 import { Exclude } from 'class-transformer';
 
 @ObjectType()
 export class User implements UserPrismaType {
+  @Field(() => String)
   id: string;
+
+  @Field(() => String)
   email: string;
+
+  @Field(() => String)
   username: string;
+
+  @Field(() => String)
   phone: string;
+
+  @Field(() => Number)
   birth_year: number;
 
   @Exclude()
