7097b80b7f3d04c36d34b480fbc8bdcfc4c81f0d
Edmund <edmundlzen@gmail.com>
2024-02-18 21:03:00 +0800

    Add PrismaService to FinancialGoalModule

--- src/financial-goal/financial-goal.module.ts
+++ src/financial-goal/financial-goal.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FinancialGoalService } from './financial-goal.service';
 import { FinancialGoalResolver } from './financial-goal.resolver';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [FinancialGoalResolver, FinancialGoalService]
+  providers: [FinancialGoalResolver, FinancialGoalService, PrismaService],
 })
 export class FinancialGoalModule {}
