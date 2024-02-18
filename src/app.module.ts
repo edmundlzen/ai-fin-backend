2b89a9d21ec84c1a2957d10511f3c278acdcadf4
Edmund <edmundlzen@gmail.com>
2024-02-18 13:30:39 +0800

    Add GraphQL configuration to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,17 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UserModule } from './user/user.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
     }),
     UserModule,
   ],
